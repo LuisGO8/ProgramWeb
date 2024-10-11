aa8f35cab638ffed83f2b06c9585ade360ffe5e0
LuisGO8 <jguizaro2200@alumno.ipn.mx>
2024-10-11 03:19:43 -0600

    Continuacion clase 10/10/2024

---
+++ Clase_26092024/script.js
@@ -0,0 +1,24 @@
+/*Ete codigo JavaScript contiene funciones que estandiseñadas para en conjunto
+controlando el comportamiento de los botones y el campo de entrada (display)
+
+
+
+*/
+function appendToDisplay (value){
+    document.getElementById('display').value += value;
+}
+function clearDisplay(){
+    document.getElementById('display').value = '';
+}
+function deleteLast(){
+    let displayValue = document.getElementById('display').value;
+    document.getElementById('display').value = displayValue.slice(0,-1);
+}
+function calculateResult(){
+    try{
+        let result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch (error){
+        alert('Expresión Invalida')
+    }
+}
