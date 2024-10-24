9d2cf7a25a391a75185e6aa64724c2bbfdcfd550
LuisGO8 <jguizaro2200@alumno.ipn.mx>
2024-10-24 16:25:17 -0600

    Arreglo

---
+++ Clase_24102024/script.js
@@ -0,0 +1,27 @@
+//Arreglos
+
+/*
+let array = ["dato 1", 1, Boolean];
+console.log("tamaño original:"+array.length);
+array.push("5");
+console.log(array[3]);
+console.log("nuevo tamaño:"+array.length);
+*/
+
+
+
+/*
+Hacer la tabla del 5
+*/
+
+
+let tabla =[];
+for(i=0; i<10; i++){
+    if(tabla.length <6){
+        tabla.push("El resultado de 5X"+i+ " = " +5*i)
+        console.log(tabla)
+    }
+    
+}
+console.log("El array tiene "+tabla.length) 
+
